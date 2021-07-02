98d38aa8b71a04949a452e8d6b3082bd1adb1535
chetan98 <rawatchetan133@gmail.com>
2021-07-03 01:55:55 +0530

    first commit

---
+++ src/main/java/com/nutrigreens/orderportal/OrderPortalApplication.java
@@ -0,0 +1,13 @@
+package com.nutrigreens.orderportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrigreens/orderportal/OrderPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrigreens.orderportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
